b56b1b62a0df6e2013acb36af53552ecac916325
Debashis Saha <debashissaha221@gmail.com>
2023-03-03 21:16:35 +0530

    changes

--- src/Components/ImageCanvas.jsx
+++ src/Components/ImageCanvas.jsx
@@ -4,6 +4,7 @@ const ImageCanvas = (props) => {
   const canvasRef = useRef(null);
   const { src, width, height } = props;
   const [canvasContext, setCanvasContext] = useState(null);
+  const [aspectRatio, setAspectRatio] = useState(0);
   const [canvas, setCanvas] = useState(null);
   const [offsetX, setOffsetX] = useState(null);
   const [offsetY, setOffsetY] = useState(null);
@@ -11,7 +12,6 @@ const ImageCanvas = (props) => {
   const pi2 = 3.14 * 2;
   const resizerRadius = 8;
   const rr = resizerRadius * resizerRadius;
-  let aspectRatio;
   let draggingResizer = -1;
   let imageX;
   let imageY;
@@ -35,7 +35,7 @@ const ImageCanvas = (props) => {
       const canvasCenterX = canvas.width / 2;
       const canvasCenterY = canvas.height / 2;
       //* Calculate the aspect ratio of the image
-      aspectRatio = img.width / img.height;
+      setAspectRatio(img.width / img.height);
 
       //* Calculate the maximum width and height that the image can be
       const maxWidth = canvas.width * 0.8;
@@ -55,14 +55,13 @@ const ImageCanvas = (props) => {
       const imgY = canvasCenterY - imgHeight / 2;
       imageX = imgX;
       imageY = imgY;
-
       imageWidth = imgWidth;
       imageHeight = imgHeight;
       imageRight = imageX + imageWidth;
       imageBottom = imageY + imageHeight;
       draw(true, false);
     };
-  }, [src]);
+  }, [src, width, height, aspectRatio]);
 
   const draw = (withAnchors, withBorders) => {
     //* clear the canvas
@@ -173,17 +172,16 @@ const ImageCanvas = (props) => {
   };
 
   const handleMouseMove = (e) => {
+    mouseX = parseInt(e.clientX - offsetX);
+    mouseY = parseInt(e.clientY - offsetY);
     if (draggingResizer > -1) {
-      mouseX = parseInt(e.clientX - offsetX);
-      mouseY = parseInt(e.clientY - offsetY);
-
       //* resize the image
       switch (draggingResizer) {
         case 0:
           console.log("resizing top left");
           //*top-left
           imageX = mouseX;
-          imageWidth = imageRight - mouseX;
+          imageWidth = imageRight - imageX;
           imageY = mouseY;
           imageHeight = imageWidth / aspectRatio;
           break;
@@ -219,14 +217,7 @@ const ImageCanvas = (props) => {
       //* set the image right and bottom
       imageRight = imageX + imageWidth;
       imageBottom = imageY + imageHeight;
-      // console.log({
-      //   imageWidth,
-      //   imageHeight,
-      //   imageX,
-      //   imageY,
-      //   imageRight,
-      //   imageBottom,
-      // });
+
       //* redraw the image with resizing anchors
       draw(true, true);
     } else if (draggingImage) {
